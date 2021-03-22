eb09c9551710ed9b2b16bcb721dfebd376aad2fb
Karic <zhangkaric@gmail.com>
2021-03-22 10:52:49 +0800

    Review Code

--- khaos/src/main/java/com/class100/khaos/KhMeetingActivity.java
+++ khaos/src/main/java/com/class100/khaos/KhMeetingActivity.java
@@ -3,9 +3,7 @@ package com.class100.khaos;
 import android.Manifest;
 import android.app.Service;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
@@ -13,15 +11,15 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.chinamobile.ysx.YSXInMeetingService;
 import com.chinamobile.ysx.YSXMobileRTCVideoUnitAspectMode;
 import com.chinamobile.ysx.YSXMobileRTCVideoUnitRenderInfo;
 import com.chinamobile.ysx.YSXMobileRTCVideoView;
 import com.chinamobile.ysx.YSXSdk;
+import com.class100.atropos.env.context.permission.AtPermission;
+import com.class100.atropos.env.context.permission.PermissionCallback;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -98,34 +96,18 @@ public class KhMeetingActivity extends AppCompatActivity {
     }
 
     private void checkPermission() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            init();
-            return;
-        }
+        AtPermission.checkPermission(this, new PermissionCallback() {
+            @Override
+            public void onGrantedEntirely() {
+                init();
+            }
 
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
-                checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO},
-                    REQUEST_CAMERA_CODE);
-        } else {
-            init();
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for (int result : grantResults) {
-            if (result != PackageManager.PERMISSION_GRANTED) {
+            @Override
+            public void onPermissionDenied(List<String> list) {
                 showPermissionHint();
                 finish();
-                return;
             }
-        }
-        init();
+        }, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO);
     }
 
     private void showPermissionHint() {
